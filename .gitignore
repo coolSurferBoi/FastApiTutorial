8799bc4086ddfbbb1f86825aac2f9fdd44cc5a04
Hoon Kim <hoondataspecialist@outlook.com>
2026-01-13 22:48:28 +1100

    To be continued

--- .gitignore
+++
@@ -1,3 +0,0 @@
-from fastapi import fastapi
-
-app = FastAPI()
---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+posts: list[dict] = [
+    {
+        "id":1,
+        "author": "Hoon Kim",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast",
+        "date_posted": "April 20, 2025"
+    },
+    {
+        "id":2,
+        "author": "Jane Doe",
+        "title": "Python is great for Web Development",
+        "content": "Python is a great language for web development, and fastAPI makes it even better!",
+        "date_posted": "April 21, 2025"
+    }
+]
+
+@app.get("/")
+def home():
+    return {"message": "Hello World!"}
+
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
