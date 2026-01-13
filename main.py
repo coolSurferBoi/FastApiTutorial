12ae5da11faf553f68ea517740e16802396f8d0d
John Doe <hoondataspecialist@outlook.com>
2026-01-14 08:34:07 +1100

    Part 4

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
-
+from utils.schemas import PostCreate,PostResponse
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -28,7 +28,11 @@ posts: list[dict] = [
     },
 ]
 
-
+"""
+----------------------------
+Webpage
+----------------------------
+"""
 @app.get("/", include_in_schema=False, name="home")
 @app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
@@ -51,19 +55,46 @@ def post_page(request: Request, post_id: int):
             )
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
+"""
+----------------------------
+APIs
+----------------------------
+"""
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model = list[PostResponse])
 def get_posts():
     return posts
 
 
-@app.get("/api/posts/{post_id}")
+@app.get("/api/posts/{post_id}", response_model = PostResponse)
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
+@app.post(
+    "/api/posts",
+    response_model=PostResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23, 2025",
+    }
+    posts.append(new_post)
+    return new_post
+
+"""
+----------------------------
+Exception Handlers
+----------------------------
+"""
 
 @app.exception_handler(StarletteHTTPException)
 def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
