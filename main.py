0edfd04c199207a32489625f6460d52fda7b7a06
Hoon Kim <hoondataspecialist@outlook.com>
2026-02-02 23:05:27 +1100

    Part 5 changes

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import FastAPI, HTTPException, Request, status, Depends
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from utils.schemas import PostCreate,PostResponse
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from schemas import PostCreate,PostResponse,UserCreate,UserResponse
+import models
+from database import Base, engine, get_db
+from typing import Annotated
+
+Base.metadata.create_all(bind=engine) #idempotent
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
+app.mount("/media", StaticFiles(directory="media"), name="media")
 templates = Jinja2Templates(directory="templates")
 
-posts: list[dict] = [
-    {
-        "id": 1,
-        "author": "Corey Schafer",
-        "title": "FastAPI is Awesome",
-        "content": "This framework is really easy to use and super fast.",
-        "date_posted": "April 20, 2025",
-    },
-    {
-        "id": 2,
-        "author": "Jane Doe",
-        "title": "Python is Great for Web Development",
-        "content": "Python is a great language for web development, and FastAPI makes it even better.",
-        "date_posted": "April 21, 2025",
-    },
-]
 
 """
 ----------------------------
@@ -65,6 +57,55 @@ APIs
 def get_posts():
     return posts
 
+@app.post(
+    "/api/users",
+    response_model=UserResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_user(user: UserCreate, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User.where(models.User.username == user)))
+    existing_user = result.scalars().first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            details = "Username already exists"
+        )
+    
+    result = db.execute(select(models.User.where(models.User.email == user.email)))
+    existing_email = result.scalars().first()
+
+    if existing_email:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            details = "Email already exists"
+        )
+    
+    new_user = models.User(
+        username = user.username,
+        email=user.email
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+@app.post(
+    "/api/users/{user_id}",
+    response_model=UserResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def get_user(user_id:int, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User.where(models.User.id == user_id)))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            details = "User does not exist"
+        )
+    return user
 
 @app.get("/api/posts/{post_id}", response_model = PostResponse)
 def get_post(post_id: int):
