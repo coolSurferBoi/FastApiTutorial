cddae8d2572f3ffb652675f5a53f6dde6f44cfd9
Hoon Kim <hoondataspecialist@outlook.com>
2026-04-27 22:33:55 +1000

    finally did something lmao

--- routers/users.py
+++ routers/users.py
@@ -11,9 +11,8 @@ from datetime import timedelta
 from fastapi.security import OAuth2PasswordRequestForm
 
 from sqlalchemy import func
-from auth import create_access_token, hash_password, oauth2_scheme, verify_access_token, verify_password
+from auth import CurrentUser, create_access_token, hash_password, verify_password, oauth2_scheme, verify_access_token
 from config import settings
-
 
 router = APIRouter()
 
@@ -84,29 +83,27 @@ async def login_for_Access_token(
     return Token(access_token=access_token, token_type = "bearer")
 
 
-
 @router.get("/me", response_model = UserPrivate)
 async def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)],
     db: Annotated[AsyncSession, Depends(get_db)]
 ):
-    """Get the Currently Authenticated User."""
     user_id = verify_access_token(token)
     if user_id is None:
         raise HTTPException(
-            status_code = status.HTTP_401_UNAUTHORIZED,
-            detail= "Invalid or expired token",
-            headers = {"WWW-Authenticate":"Bearer"}
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate":"Bearer"}
         )
-    
-    # Validate user_id is a valid integer (Defense against malformed jwt)
+
+    # validate user_id is a valid integer (Defense against malformed JWT)
     try:
         user_id_int = int(user_id)
     except (TypeError, ValueError):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or expired token",
-            headers={"WWW-Authenticate": "Bearer"}
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid or expired token",
+            headers={"WWW-Authenticate":"Bearer"}
         )
     
     result = await db.execute(
@@ -115,9 +112,9 @@ async def get_current_user(
     user = result.scalars().first()
     if not user:
         raise HTTPException(
-            status_code = status.HTTP_401_UNAUTHORIZED,
-            detail = "User not found",
-            headers={"WWW-Authenticate": "Bearer"}
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
+            headers={"WWW-Authenticate":"Bearer"},
         )
     return user
 
@@ -151,8 +148,15 @@ async def get_user_posts(user_id:int, db:Annotated[AsyncSession, Depends(get_db)
     return posts
 
 @router.patch("/{user_id}", response_model=UserPrivate)
-async def update_user(user_id:int,user_update: UserUpdate, db:Annotated[AsyncSession, Depends(get_db)]):
-    result = await db.execute(select(models.User).where(models.User.id == user_id))
+async def update_user(user_id:int,user_update: UserUpdate, current_user: CurrentUser, db:Annotated[AsyncSession, Depends(get_db)]):
+    
+    if user_id!= current_user.id:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "Not authorized to  update this user"
+        )
+    
+    result = await db.execute(select(models.User).where(models.User.id == current_user))
     user = result.scalars().first()
     if not user:
         raise HTTPException(
@@ -194,7 +198,14 @@ async def update_user(user_id:int,user_update: UserUpdate, db:Annotated[AsyncSes
     return user
 
 @router.delete("/{user_id}", status_code = status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+async def delete_user(user_id: int,current_user:CurrentUser, db: Annotated[AsyncSession, Depends(get_db)]):
+    
+    if user_id!= current_user.id:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "Not authorized to delete this user"
+        )
+    
     result = await db.execute(select(models.Post).where(models.User.id == user_id))
     user = result.scalars().first()
     if not user:
--- schemas.py
+++ schemas.py
@@ -6,9 +6,15 @@ class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length= 50)
     email: EmailStr = Field(max_length=120)
 
-
 class UserCreate(UserBase):
     password:str = Field(min_length=8)
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    image_file: str | None
+    image_path: str
 
 class UserPublic(BaseModel):
     
@@ -20,7 +26,6 @@ class UserPublic(BaseModel):
     image_path: str
 
 class UserPrivate(UserPublic):
-
     email: EmailStr
     
 class UserUpdate(BaseModel):
@@ -32,14 +37,13 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
-
 class PostBase(BaseModel):
 
     title: str = Field(min_length=1,max_length=100)
     content: str = Field(min_length=1)
     
 class PostCreate(PostBase):
-    user_id: int # Temporary
+    user_id: int
 
 class PostUpdate(BaseModel):
     title: str | None = Field(default = None, min_length= 1, max_length= 100)
